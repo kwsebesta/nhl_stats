c8895e2d81d37d100f3dfd61678195e6e0394859
kwsebesta <nivek@Kevins-MacBook-Air.local>
2019-02-06 10:53:41 -0600

    add sqlcmds.py and update .gitignore with sql db

---
+++ sqlcmds.py
@@ -0,0 +1,31 @@
+"""module for common sql commands"""
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as err:
+        print(err)
+
+    return None
+
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return: None
+    """
+    try:
+        cur = conn.cursor()
+        cur.execute(create_table_sql)
+    except Error as err:
+        print(err)
